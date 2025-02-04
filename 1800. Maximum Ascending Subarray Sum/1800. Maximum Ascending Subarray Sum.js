20f19c9b1c5a630c0cb0175e1015b12de59e5419
Muhammed Haneen <haneenep134@gmail.com>
2025-02-04 07:14:49 +0530

    nw prblm

---
+++ 1800. Maximum Ascending Subarray Sum/1800. Maximum Ascending Subarray Sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function (nums) {
+    let maxSum = nums[0];
+    let currentSum = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] <= nums[i - 1]) {
+            currentSum = nums[i]
+        } else {
+            currentSum += nums[i]
+        }
+        maxSum = Math.max(currentSum, maxSum)
+    }
+    return maxSum
+};
