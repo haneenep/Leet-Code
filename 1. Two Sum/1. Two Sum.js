673799eb7eb340c4c997a31fcd466f17bfe71b92
Muhammed Haneen <haneenep134@gmail.com>
2025-02-14 11:40:48 +0530

    new problem

---
+++ 1. Two Sum/1. Two Sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const num = target - nums[i];
+        if (map.has(num)) {
+            return [map.get(num), i];
+        }
+        map.set(nums[i], i)
+    }
+};
