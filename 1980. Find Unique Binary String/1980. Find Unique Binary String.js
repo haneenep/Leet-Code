83688c9d17110e0fb2c84ea500f8200a1f8333c1
Muhammed Haneen <haneenep134@gmail.com>
2025-02-20 11:28:40 +0530

    new problem

---
+++ 1980. Find Unique Binary String/1980. Find Unique Binary String.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} nums
+ * @return {string}
+ */
+var findDifferentBinaryString = function (nums) {
+    let n = nums[0].length;
+    let res = '';
+    for(let i = 0; i < n; i++){
+       res += nums[i][i] === '0' ? '1' : '0';
+    }
+    return res;
+};
