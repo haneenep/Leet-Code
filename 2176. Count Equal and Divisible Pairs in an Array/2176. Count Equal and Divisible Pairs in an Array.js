a657352cc89dd87247c0a9c7b786a133e8d9887e
Muhammed Haneen <haneenep134@gmail.com>
2025-04-17 11:45:52 +0530

    2176: solution changed to more optimized

--- 2176. Count Equal and Divisible Pairs in an Array/2176. Count Equal and Divisible Pairs in an Array.js
+++ 2176. Count Equal and Divisible Pairs in an Array/2176. Count Equal and Divisible Pairs in an Array.js
@@ -5,12 +5,25 @@
  */
 var countPairs = function (nums, k) {
     let count = 0;
+    let map = new Map();
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === nums[j] && i * j % k === 0) {
-                count++
+
+        if (map.has(nums[i])) {
+
+            for (let index of map.get(nums[i])) {
+                if ((index * i) % k === 0) {
+                    count++
+                }
             }
+
+        } else {
+            map.set(nums[i], []);
         }
+        map.get(nums[i]).push(i);
+
     }
+
     return count;
+
 };
