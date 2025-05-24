d480bb555b4da86936be18dbb37fc23ddbe58735
haneenep <haneenep134@gmail.com>
2025-05-24 14:34:57 +0530

    problem add

---
+++ 2000. Reverse Prefix of Word/2000. Reverse Prefix of Word.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} word
+ * @param {character} ch
+ * @return {string}
+ */
+var reversePrefix = function (word, ch) {
+    const index = word.indexOf(ch);
+    return index === -1 ? word : word.slice(0, index + 1).split('').reverse().join('') + word.slice(index + 1);
+};
